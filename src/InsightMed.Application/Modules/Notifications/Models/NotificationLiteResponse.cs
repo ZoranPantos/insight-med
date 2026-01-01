73652b9c96daab2b151cfc5ee30a2e0f6a68c43a
zoran.pantos <pantos78zoran@gmail.com>
2026-01-01 17:28:16 +0100

    chore: include notification id in get all endpoint

--- src/InsightMed.Application/Modules/Notifications/Models/NotificationLiteResponse.cs
+++ src/InsightMed.Application/Modules/Notifications/Models/NotificationLiteResponse.cs
@@ -2,6 +2,7 @@
 
 public sealed class NotificationLiteResponse
 {
+    public int Id { get; set; }
     public string Message { get; set; } = string.Empty;
     public int LabReportId { get; set; }
 }
