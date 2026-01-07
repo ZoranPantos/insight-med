0fdd4fde923aa635215374780eeaf0bbc921c71e
zoran.pantos <pantos78zoran@gmail.com>
2026-01-07 18:05:17 +0100

    feat: add validator for register command

---
+++ src/InsightMed.Application/Auth/Validation/RegisterCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using InsightMed.Application.Auth.Commands;
+
+namespace InsightMed.Application.Auth.Validation;
+
+public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(command => command)
+            .NotEmpty();
+
+        RuleFor(command => command.Email)
+            .NotEmpty();
+
+        RuleFor(command => command.Password)
+            .NotEmpty();
+    }
+}
