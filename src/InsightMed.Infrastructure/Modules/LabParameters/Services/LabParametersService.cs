19530cb1b44ad44ca5aa56042e5cd0c7b5bc99dd
zoran.pantos <pantos78zoran@gmail.com>
2026-01-14 17:14:55 +0100

    chore: sort lab parameters

--- src/InsightMed.Infrastructure/Modules/LabParameters/Services/LabParametersService.cs
+++ src/InsightMed.Infrastructure/Modules/LabParameters/Services/LabParametersService.cs
@@ -16,6 +16,7 @@ public sealed class LabParametersService : ILabParametersService
     {
         return await _context.LabParameters
             .AsNoTracking()
+            .OrderBy(labParameter => labParameter.Name)
             .ToListAsync()
             .ConfigureAwait(false);
     }
